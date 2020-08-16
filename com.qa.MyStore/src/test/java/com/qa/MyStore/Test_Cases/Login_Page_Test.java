2ba82865e99636f331f7e2b6b90308baecf34760
Harshad Bele <beleharshad@gmail.com>
2020-08-16 21:31:42 +0530

    added new method logout_Test to branch_B

--- com.qa.MyStore/src/test/java/com/qa/MyStore/Test_Cases/Login_Page_Test.java
+++ com.qa.MyStore/src/test/java/com/qa/MyStore/Test_Cases/Login_Page_Test.java
@@ -40,7 +40,9 @@ public class Login_Page_Test extends Test_Base{
 	public void ValidateLogo_Display_Test()
 	{
 		Assert.assertTrue(loginPage.ValidateLogo_Display());
+		System.out.println("branch B");
 	}
+	
 	
 	@Test(priority=3)
 	public void login_Test()
@@ -48,6 +50,14 @@ public class Login_Page_Test extends Test_Base{
 		homePage.login_username_link();
 		homePage=loginPage.login(prop.getProperty("username"),prop.getProperty("password"));
 	}
+	
+	//logo validation merging branch_B & branch_C
+		@Test(priority=2)
+		public void logout_Test()
+		{
+			Assert.assertTrue(loginPage.ValidateLogo_Display());
+			System.out.println("branch B");
+		}
 	
 	@AfterMethod
 	public void teardown()
